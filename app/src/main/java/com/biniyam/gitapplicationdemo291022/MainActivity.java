65095620c666ab79944ed32a82e8e9c865968958
bygxs <114972065+bygxs@users.noreply.github.com>
2022-10-29 22:11:01 +0200

    20:08 committing now... Just changed the annoying Hello World to Hello  Git in .xml with a string id and made a java  sout message for the hell of it.

---
+++ app/src/main/java/com/biniyam/gitapplicationdemo291022/MainActivity.java
@@ -0,0 +1,20 @@
+package com.biniyam.gitapplicationdemo291022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("This is my Git projcet 2.0 attempt to save Git locally " +
+                "22:07 saturday 29 Oct 2022");
+
+
+
+    }
+}
