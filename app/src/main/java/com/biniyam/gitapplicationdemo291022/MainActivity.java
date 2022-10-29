bb31f5db29b17e48bfe9549c012bcab60e5d3cdc
bygxs <114972065+bygxs@users.noreply.github.com>
2022-10-29 22:44:41 +0200

    22:44 this how i love my self

--- app/src/main/java/com/biniyam/gitapplicationdemo291022/MainActivity.java
+++ app/src/main/java/com/biniyam/gitapplicationdemo291022/MainActivity.java
@@ -3,8 +3,15 @@ package com.biniyam.gitapplicationdemo291022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView helloText;
+    Button git;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("This is my Git projcet 2.0 attempt to save Git locally " +
                 "22:07 saturday 29 Oct 2022");
+        helloText = findViewById(R.id.helloText);
 
+        git = findViewById(R.id.button2just4Git);
+        git.setOnClickListener((View.OnClickListener) helloText);
+
+       /* git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+*/
 
 
     }
